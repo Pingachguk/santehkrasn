b5fd8ff495da946ea65ab272bdb3f17634239c01
Pingachguk <izvrakzuckercat@gmail.com>
2021-10-08 20:01:09 +0300

    upd

--- bot/main.py
+++ bot/main.py
@@ -32,12 +32,13 @@ async def on_shutdown(dp):
 
 
 if __name__ == '__main__':
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path=config.WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=config.WEBAPP_HOST,
-        port=config.WEBAPP_PORT,
-    )
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    # executor.start_webhook(
+    #     dispatcher=dp,
+    #     webhook_path=config.WEBHOOK_PATH,
+    #     on_startup=on_startup,
+    #     on_shutdown=on_shutdown,
+    #     skip_updates=True,
+    #     host=config.WEBAPP_HOST,
+    #     port=config.WEBAPP_PORT,
+    # )
