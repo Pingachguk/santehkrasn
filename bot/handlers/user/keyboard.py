57c8ec4f5edbe2f82ba0ac7848ef1595b39eb33c
Pingachguk <izvrakzuckercat@gmail.com>
2021-10-08 20:19:37 +0300

    upd

--- bot/handlers/user/keyboard.py
+++ bot/handlers/user/keyboard.py
@@ -5,4 +5,5 @@ def start_keyboard():
     markup = types.ReplyKeyboardMarkup()
     markup.add(types.KeyboardButton("📝 Заказать услуги сантехника"))
     markup.add(types.KeyboardButton("🔎 Узнать цены на услуги"))
+    markup.add(types.KeyboardButton("📰 Канал с выполненными работами"))
     return markup
